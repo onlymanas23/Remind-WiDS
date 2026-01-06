a186bcde035c292acd2773d4fc1ced963cb664fe
onlymanas23 <onlymanas23@gmail.com>
2026-01-06 20:04:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from google import genai
+from google . genai import types
+from dotenv import load_dotenv
+import json
+import os
+from datetime import datetime  
+st.title("Test App")
+st.write("If you see this, Streamlit is working")
